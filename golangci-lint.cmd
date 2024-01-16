9257d3cb7a0ef2903f9d6c3281d594dd520bb9b9
Balyuk Dmitrii <Balyuk-D@datagile.ru>
2024-01-16 11:02:48 +0300

    fix: cleared main

--- golangci-lint.cmd
+++
@@ -1,3 +0,0 @@
-docker run --rm -v %cd%:/app -w /app golangci/golangci-lint:v1.53.3 golangci-lint run -c .golangci.yml > ./golangci-lint/report-unformatted.json
-docker run --rm -v %cd%:/app imega/jq -c . /app/golangci-lint/report-unformatted.json > ./golangci-lint/report.json
-pause
